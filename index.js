b032295c4c5dc595f13d3c0571dfd3bba4bd1a02
Alexey Osipenko <alexey.osipenko@gmail.com>
2016-10-08 15:53:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.log("Request for host " + request.headers.host);
-  response.send('Hello some another branch for all!')
+  response.send('Hello some another branch for all!!!')
 })
 
 app.listen(app.get('port'), function() {
